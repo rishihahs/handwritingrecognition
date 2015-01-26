060b7315c732ca556557902d2a0089bf20a02f23
Rishi Shah <rishihahs@gmail.com>
2015-01-26 15:18:46 -0600

    Conventionalize the names

--- handwritingrecognition/data.py
+++ handwritingrecognition/data.py
@@ -13,19 +13,19 @@ def loaddata(directory):
     global X_train, X_cv, X_test, Y_train, Y_cv, Y_test
 
     if not X_train:
-        X, y = _extract_data(directory)
+        X, y = __extract_data(directory)
 
         # 60% of data is for training
         mainlen, otherlen = math.floor(0.6 * len(X)), math.ceil(0.2 * len(X))
 
         X_train, X_cv, X_test = X[0:mainlen], X[mainlen:mainlen + otherlen], X[mainlen + otherlen:]
 
-        Y = _multify(y)
+        Y = __multify(y)
         Y_train, Y_cv, Y_test = Y[0:mainlen], Y[mainlen:mainlen + otherlen], Y[mainlen + otherlen:]
 
     return collections.namedtuple('Data', 'X_train X_cv X_test Y_train Y_cv Y_test')(X_train, X_cv, X_test, Y_train, Y_cv, Y_test)
 
-def _multify(y):
+def __multify(y):
     ynew = numpy.zeros((len(y), 26)) # 26 letters of the alphabet
     alphabet = string.ascii_lowercase
 
@@ -34,7 +34,7 @@ def _multify(y):
         ynew[i, index] = 1
     return ynew
 
-def _extract_data(directory):
+def __extract_data(directory):
     # Load data into example vector
     data = numpy.load(os.path.abspath(directory))
 

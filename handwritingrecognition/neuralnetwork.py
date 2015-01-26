bc6bd9f3eb65737faaa52887ba1f42514a9f7561
Rishi Shah <rishihahs@gmail.com>
2015-01-26 15:20:40 -0600

    Cost function

--- handwritingrecognition/neuralnetwork.py
+++ handwritingrecognition/neuralnetwork.py
@@ -19,19 +19,31 @@ class NeuralNetwork(object):
             epsilon = math.sqrt(6) / math.sqrt(layers[i] + layers[i + 1]) # Statistically good epsilon
             self.Theta.append((2 * epsilon) * numpy.random.random_sample((layers[i + 1], layers[i] + 1)) - epsilon) # Randomized Theta
 
+    def __cost(self):
+        a, z = self.__forwardpropogation()
+
+        J = numpy.sum(-1 * self.Y * numpy.log(a[-1]))
+        J = J + numpy.sum(-1 * (1 - self.Y) * numpy.log(1 - a[-1]))
+        J = 1/self.X.shape[0] * J
+
+        return J
+
     def __forwardpropogation(self):
         a = []
         z = [None] # First element is None, since z starts at layer 2
 
         a1 = self.X
-        a1 = numpy.hstack((numpy.ones((a1.shape[0], 1), dtype=a1.dtype),  a1)) # Prepend columns with ones
+        a1 = numpy.hstack((numpy.ones((a1.shape[0], 1), dtype=numpy.float64),  a1)) # Prepend columns with ones
         a.append(a1)
 
         for i in range(len(self.Theta)):
             zcurrent = numpy.dot(a[i], numpy.transpose(self.Theta[i]))
             z.append(zcurrent)
             acurrent = self.__sigmoid(zcurrent)
-            acurrent = numpy.hstack((numpy.ones((acurrent.shape[0], 1), dtype=acurrent.dtype),  acurrent)) # Prepend columns with ones
+
+            if i < len(self.Theta) - 1:
+                acurrent = numpy.hstack((numpy.ones((acurrent.shape[0], 1), dtype=acurrent.dtype),  acurrent)) # Prepend columns with ones
+
             a.append(acurrent)
 
         return (a, z)
@@ -40,7 +52,7 @@ class NeuralNetwork(object):
         return 1 / (1 + numpy.exp(-1 * z))
 
     def prints(self):
-        print(self.__forwardpropogation())
+        print(self.__cost())
 
 if __name__ == '__main__':
     stuff = data.loaddata(sys.argv[1])

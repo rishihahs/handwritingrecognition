a3ba3a2abd94fd5ee1ba5ff648b871f42bc40807
Rishi Shah <rishihahs@gmail.com>
2015-01-25 23:00:04 -0600

    Forward Propagation

--- handwritingrecognition/neuralnetwork.py
+++ handwritingrecognition/neuralnetwork.py
@@ -12,10 +12,32 @@ class NeuralNetwork(object):
         self.Y = Y
 
         # Create Theta
-        thetacolumns = max(X.shape[1], max(layers)) # Maximum of all layer units
-        epsilon = math.sqrt(6) / math.sqrt(layers[-2] + layers[-1]) # Statistically good epsilon
-        self.epsilon = epsilon
-        self.Theta = (2 * epsilon) * numpy.random.random_sample((len(layers), thetacolumns)) - epsilon # Randomized Theta
+        self.Theta = []
+        layers.insert(0, X.shape[1])
+
+        for i in range(len(layers) - 1):
+            epsilon = math.sqrt(6) / math.sqrt(layers[i] + layers[i + 1]) # Statistically good epsilon
+            self.Theta.append((2 * epsilon) * numpy.random.random_sample((layers[i + 1], layers[i] + 1)) - epsilon) # Randomized Theta
+
+    def __forwardpropogation(self):
+        a = []
+        z = [None] # First element is None, since z starts at layer 2
+
+        a1 = self.X
+        a1 = numpy.hstack((a1, numpy.ones((a1.shape[0], 1), dtype=a1.dtype))) # Prepend columns with ones
+        a.append(a1)
+
+        for i in range(len(self.Theta)):
+            zcurrent = numpy.dot(a[i], numpy.transpose(self.Theta[i]))
+            z.append(zcurrent)
+            acurrent = self.__sigmoid(zcurrent)
+            acurrent = numpy.hstack((acurrent, numpy.ones((acurrent.shape[0], 1), dtype=acurrent.dtype))) # Prepend columns with ones
+            a.append(acurrent)
+
+        return (a, z)
+
+    def __sigmoid(self, z):
+        return 1 / (1 + numpy.exp(-1 * z))
 
 if __name__ == '__main__':
     stuff = data.loaddata(sys.argv[1])

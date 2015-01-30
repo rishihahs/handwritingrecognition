3f8ed462339971a4d2484bf83eb2b0b53c0caa5e
Rishi Shah <rishihahs@gmail.com>
2015-01-29 23:47:32 -0600

    Neuralnetwork changes and metrics

---
+++ handwritingrecognition/metrics.py
@@ -0,0 +1,59 @@
+import matplotlib.pyplot
+import numpy
+
+import neuralnetwork
+import data
+import sys
+
+def learningcurves(layers, X_train, Y_train, X_cv, Y_cv, lambda_regularization):
+    trainplotx = []
+    trainploty = []
+    cvplotx = []
+    cvploty = []
+
+    trainlen = len(X_train)
+    for i in reversed(range(30)):
+        Theta = neuralnetwork.randomtheta(layers, X_train.shape[1], maxiter = 63)
+        output = neuralnetwork.train(X_train[(trainlen - trainlen/(i + 1)):],
+                               Y_train[(trainlen - trainlen/(i + 1)):],
+                                     Theta,
+                                     lambda_regularization)
+        trainplotx.append(trainlen/(i + 1))
+        trainploty.append(output[1])
+
+        cost = neuralnetwork.calculatecost(X_cv, Y_cv, output[0], Theta, lambda_regularization)
+        cvplotx.append(trainlen/(i + 1))
+        cvploty.append(cost)
+
+    print(trainplotx)
+    print(trainploty)
+    print(cvplotx)
+    print(cvploty)
+    matplotlib.pyplot.plot(trainplotx, trainploty)
+    matplotlib.pyplot.plot(cvplotx, cvploty)
+    matplotlib.pyplot.show()
+
+def lambda_check(layers, X_train, Y_train, X_cv, Y_cv):
+    lambdas = [0, 0.01, 0.02, 0.04, 0.08, 0.16, 0.32, 0.64, 1.28, 2.56, 5.12, 10.24]
+    costs = []
+    for lambda_regularization in lambdas:
+        Theta = neuralnetwork.randomtheta(layers, X_train.shape[1])
+        output = neuralnetwork.train(X_train,
+                                     Y_train,
+                                     Theta,
+                                     lambda_regularization)
+
+        cost = neuralnetwork.calculatecost(X_cv, Y_cv, output[0], Theta, 0)
+        costs.append(cost)
+
+    print(costs)
+    print()
+    print("Min Cost Is:")
+    print(lambdas[numpy.argmin(costs)])
+    matplotlib.pyplot.plot(lambdas, costs)
+    matplotlib.pyplot.show()
+
+if __name__ == '__main__':
+    stuff = data.loaddata(sys.argv[1])
+    learningcurves([63, 36, 26], stuff.X_train, stuff.Y_train, stuff.X_cv, stuff.Y_cv, 0.16)
+    # lambda_check([63, 36, 26], stuff.X_train, stuff.Y_train, stuff.X_cv, stuff.Y_cv)
--- handwritingrecognition/neuralnetwork.py
+++ handwritingrecognition/neuralnetwork.py
@@ -5,112 +5,140 @@ import sys
 
 import data
 
-class NeuralNetwork(object):
+__memoizeforward = {} # Memoize forwardpropogation
 
-    def __init__(self, layers, X, Y): # layers = [1 2 3] where the elements are number of units
-        self.layers = layers
-        self.X = X
-        self.Y = Y
+def randomtheta(layers, num_features, maxiter = 600):
+    # Create Theta
+    Theta = []
+    layers = list(layers)
+    layers.insert(0, num_features)
 
-        # Create Theta
-        self.Theta = []
-        layers.insert(0, X.shape[1])
+    for i in range(len(layers) - 1):
+        epsilon = math.sqrt(6) / math.sqrt(layers[i] + layers[i + 1]) # Statistically good epsilon
+        Theta.append((2 * epsilon) * numpy.random.random_sample((layers[i + 1], layers[i] + 1)) - epsilon) # Randomized Theta
 
-        for i in range(len(layers) - 1):
-            epsilon = math.sqrt(6) / math.sqrt(layers[i] + layers[i + 1]) # Statistically good epsilon
-            self.Theta.append((2 * epsilon) * numpy.random.random_sample((layers[i + 1], layers[i] + 1)) - epsilon) # Randomized Theta
+    return Theta
 
-        # Forward propogation memoization
-        self.memoizeforward = {}
+def train(X, Y, Theta, lambda_regularization):
+    output = optimize.fmin_l_bfgs_b((lambda x: __min_cost(x, X, Y, Theta, lambda_regularization)),
+                                    __unroll(Theta),
+                                    (lambda x: __min_gradient(x, X, Y, Theta, lambda_regularization)),
+                                    maxiter)
+    return output
 
-    def train(self):
-        # output = optimize.check_grad((lambda x: self.__cost(*self.__forwardpropogation(self.__reroll(x, self.Theta)))),
-        #                             (lambda x: self.__backpropogation(*self.__forwardpropogation(self.__reroll(x, self.Theta)))),
-        #                             self.__unroll(self.Theta))
-        output = optimize.fmin_l_bfgs_b((lambda x: self.__cost(*self.__forwardpropogation(self.X, self.__reroll(x, self.Theta)))),
-                                        self.__unroll(self.Theta),
-                                        (lambda x: self.__backpropogation(*self.__forwardpropogation(self.X, self.__reroll(x, self.Theta)))),
-                                        maxiter = 600)
+def __min_cost(x, X, Y, Theta, lambda_regularization):
+    rerolled = __reroll(x, Theta)
+    return __cost(X, Y, rerolled, lambda_regularization, *__forwardpropogation(X, rerolled))
 
-        return output
+def __min_gradient(x, X, Y, Theta, lambda_regularization):
+    rerolled = __reroll(x, Theta)
+    return __backpropogation(X, Y, rerolled, lambda_regularization, *__forwardpropogation(X, rerolled))
 
-    def __reroll(self, unrolled, target):
-        arr = []
-        pos = 0
-        for element in target:
-            arr.append(unrolled[pos:pos+element.size].reshape(element.shape))
-            pos += element.size
-        return arr
+def __reroll(unrolled, target):
+    arr = []
+    pos = 0
+    for element in target:
+        arr.append(unrolled[pos:pos+element.size].reshape(element.shape))
+        pos += element.size
+    return arr
 
-    def __unroll(self, matarray):
-        newcpy = []
-        for m in matarray:
-            newcpy.append(m.ravel())
-        return numpy.concatenate(newcpy)
+def __unroll(matarray):
+    newcpy = []
+    for m in matarray:
+        newcpy.append(m.ravel())
+    return numpy.concatenate(newcpy)
 
-    def __cost(self, a, z):
-        J = numpy.sum(-1 * self.Y * numpy.log(a[-1]))
-        J = J + numpy.sum(-1 * (1 - self.Y) * numpy.log(1 - a[-1]))
-        J = 1/self.X.shape[0] * J
+def calculatecost(X, Y, Theta, targettheta, lambda_regularization):
+    rerolled = __reroll(Theta, targettheta)
+    return __cost(X, Y, rerolled, lambda_regularization, *__forwardpropogation(X, rerolled))
 
-        print(J)
+def __cost(X, Y, Theta, lambda_regularization, a, z):
+    J = numpy.sum(-1 * Y * numpy.log(a[-1]))
+    J = J + numpy.sum(-1 * (1 - Y) * numpy.log(1 - a[-1]))
+    J = 1/X.shape[0] * J
 
-        return J
+    # Regularization
+    reg = 0
+    for theta in Theta:
+        reg += numpy.sum(theta[0:,1:].ravel() ** 2)
 
-    def __backpropogation(self, a, z):
-        delta = [a[-1] - self.Y]
-        for i in range(len(z) - 2):
-            dot = numpy.dot(delta[0], numpy.delete(self.Theta[-1 - i], 0, 1))
-            delta.insert(0, dot * self.__sigmoidGradient(z[-2 - i]))
+    J = J + (lambda_regularization / (2 * X.shape[0])) * reg
 
-        Delta = []
-        for i in range(len(delta)):
-            shapeddelta = numpy.transpose(1/self.X.shape[0] * numpy.dot(numpy.transpose(a[i]), delta[i]))
-            Delta.append(shapeddelta.ravel())
+    print(J)
 
-        return numpy.concatenate(Delta)
+    return J
 
-    def __forwardpropogation(self, X, Theta):
-        hashkey = hash(str(Theta))
-        if hashkey in self.memoizeforward:
-            val = self.memoizeforward[hashkey]
-            del self.memoizeforward[hashkey]
-            return val
+def __backpropogation(X, Y, Theta, lambda_regularization, a, z):
+    delta = [a[-1] - Y]
+    for i in range(len(z) - 2):
+        dot = numpy.dot(delta[0], numpy.delete(Theta[-1 - i], 0, 1))
+        delta.insert(0, dot * __sigmoidGradient(z[-2 - i]))
 
-        a = []
-        z = [None] # First element is None, since z starts at layer 2
+    Delta = []
+    for i in range(len(delta)):
+        shapeddelta = numpy.transpose(1/X.shape[0] * numpy.dot(numpy.transpose(a[i]), delta[i]))
 
-        a1 = X
-        a1 = numpy.hstack((numpy.ones((a1.shape[0], 1), dtype=numpy.float64),  a1)) # Prepend columns with ones
-        a.append(a1)
+        # Regularization
+        thetamod = numpy.copy(Theta[i])
+        thetamod[:, 0] = 0
+        shapeddelta = shapeddelta + (lambda_regularization / X.shape[0]) * thetamod
 
-        for i in range(len(Theta)):
-            zcurrent = numpy.dot(a[i], numpy.transpose(Theta[i]))
-            z.append(zcurrent)
-            acurrent = self.__sigmoid(zcurrent)
+        Delta.append(shapeddelta.ravel())
 
-            if i < len(Theta) - 1:
-                acurrent = numpy.hstack((numpy.ones((acurrent.shape[0], 1), dtype=acurrent.dtype),  acurrent)) # Prepend columns with ones
+    return numpy.concatenate(Delta)
 
-            a.append(acurrent)
+def __forwardpropogation(X, Theta):
+    global __memoizeforward
 
-        retval = (a, z)
-        self.memoizeforward[hashkey] = retval
+    hashkey = hash(str(Theta))
+    if hashkey in __memoizeforward:
+        val = __memoizeforward[hashkey]
+        del __memoizeforward[hashkey]
+        return val
 
-        return retval
+    a = []
+    z = [None] # First element is None, since z starts at layer 2
 
-    def __sigmoid(self, z):
-        return 1 / (1 + numpy.exp(-1 * z))
+    a1 = X
+    a1 = numpy.hstack((numpy.ones((a1.shape[0], 1), dtype=numpy.float64),  a1)) # Prepend columns with ones
+    a.append(a1)
 
-    def __sigmoidGradient(self, z):
-        g = self.__sigmoid(z)
-        return g * (1 - g)
+    for i in range(len(Theta)):
+        zcurrent = numpy.dot(a[i], numpy.transpose(Theta[i]))
+        z.append(zcurrent)
+        acurrent = __sigmoid(zcurrent)
+
+        if i < len(Theta) - 1:
+            acurrent = numpy.hstack((numpy.ones((acurrent.shape[0], 1), dtype=acurrent.dtype),  acurrent)) # Prepend columns with ones
+
+        a.append(acurrent)
+
+    retval = (a, z)
+    __memoizeforward[hashkey] = retval
+
+    return retval
+
+def __sigmoid(z):
+    # Piecewise function to handle two extremes of exponential
+    return numpy.piecewise(z,
+                           [z >= 0, z < 0],
+                           [(lambda x: 1 / (1 + numpy.exp(-1 * x))),
+                            (lambda x: numpy.exp(x) / (1 + numpy.exp(x)))])
+
+def __sigmoidGradient(z):
+    g = __sigmoid(z)
+    return g * (1 - g)
 
 if __name__ == '__main__':
     stuff = data.loaddata(sys.argv[1])
     print('Loaded Data')
-    neuralnetwork = NeuralNetwork([63, 36, 26], stuff.X_train, stuff.Y_train)
-    # neuralnetwork = NeuralNetwork([5, 3], numpy.array([[0.0312, 0.1392, 0.0246], [0.01342, 0.1322, 0.023456], [0.02943, 0.1632, 0.04654], [0.02333, 0.124352, 0.023432]]), numpy.array([[0, 1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0]]))
-    output = neuralnetwork.train()
+    # neuralnetwork = NeuralNetwork([63, 36, 26], stuff.X_train, stuff.Y_train)
+    # X = numpy.array([[0.0312, 0.1392, 0.0246], [0.01342, 0.1322, 0.023456], [0.02943, 0.1632, 0.04654], [0.02333, 0.124352, 0.023432]])
+    # Y = numpy.array([[0, 1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0]])
+    # Theta = randomtheta([5, 3], X.shape[1])
+    # lambda_regularization = 0.2
+    # output = optimize.check_grad((lambda x: __min_cost(x, X, Y, Theta, lambda_regularization)),
+    #                             (lambda x: __min_gradient(x, X, Y, Theta, lambda_regularization)),
+    #                             __unroll(Theta))
 
-    print('Testing')
+    # print(output)
